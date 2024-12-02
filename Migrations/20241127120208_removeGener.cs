329fb5eb808a392eafd334a2637abcdb26e61f19
Nahid <syed.ah.nahid@gmail.com>
2024-12-02 15:52:19 +0600

    remoe gener

---
+++ Migrations/20241127120208_removeGener.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnimeReviewAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeGener : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Animes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Animes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
